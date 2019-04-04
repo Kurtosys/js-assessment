8c690c940755af6e51001fa9336d74651641a923
Sean Jonker <Sean.jonker@kurtosys.com>
2019-04-04 08:44:34 +0200

    Objects

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+		return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+		constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+		const props = Object.keys(obj);
+		const retArr = [];
+		props.forEach((elem) => {
+			if (obj.hasOwnProperty(elem)) {
+				retArr.push(`${elem}: ${obj[elem]}`);
+			}
+		});
+		return retArr;
   }
 };
