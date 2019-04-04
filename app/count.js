fc35b8b5b323d575bad291e644956399a443270e
Sean Jonker <Sean.jonker@kurtosys.com>
2019-04-04 08:43:48 +0200

    count

--- app/count.js
+++ app/count.js
@@ -2,8 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-		for(let i = start, i <= end; i++) {
+		console.log(start);
 
+		const interval = setInterval(() => {
+			start++;
+			console.log(start);
+			if (start >= end) {
+				clearInterval(interval);
+			}
+		}, 100);
+
+		return {
+				cancel: () => {
+				clearInterval(interval);
+			}
 		}
-  }
+	},
 };
