e9521545b9d42f175577e4094764f8c4bc0001cb
Sean Jonker <Sean.jonker@kurtosys.com>
2019-04-04 08:44:21 +0200

    Better Example of module

--- app/modules.js
+++ app/modules.js
@@ -11,3 +11,14 @@ exports.modulesAnswers = {
 		}
 	}
 };
+
+hello = {
+	somethingElse: function(str1, str2) {
+		this.name = str1;
+		this.greeting = str2;
+		this.sayIt = () => {
+			return this.greeting + ', ' + this.name;
+		};
+		return this;
+	}
+}
