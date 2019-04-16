66d45406d55943413130346f76ad4fcdb34240ee
Sean Jonker <Sean.jonker@kurtosys.com>
2019-04-16 09:12:11 +0200

    Regex and strings

--- app/regex.js
+++ app/regex.js
@@ -2,26 +2,37 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+		return RegExp('\\d', 'g').test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
+		for (let i = 0; i < str.length; i++) {
+			if (!RegExp('\\d').test(str[i])) {
+				const regex = RegExp(`${str[i]}{2,}`, 'g');
+				const res = regex.test(str);
+				if (res) {
+					return res;
+					break;
+				}
+			}
+		}
+		return false;
   },
 
   endsWithVowel: function(str) {
-
+		return RegExp('[aeiou]$', 'i').test(str);
   },
 
   captureThreeNumbers: function(str) {
-
+		const out = str.match(/\d{3}/g);
+		return out ? out[0] : false;
   },
 
   matchesPattern: function(str) {
-
+		return RegExp('^\\d{3}-\\d{3}-\\d{4}$').test(str);
   },
 
   isUSD: function(str) {
-
+		return RegExp('^\\$\\d{1,3}(,\\d{3}|)+(\\.\\d{2}|)$').test(str);
   }
 };
--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,73 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+		let out = '';
+		str.split('').forEach((char) => {
+			if (!RegExp(`${char}{${amount}}$`).test(out)){
+				out += char;
+			}
+		});
+		return out;
   },
 
   wordWrap: function(str, cols) {
+		let out = '';
 
+		const findSpace = (str, from, reverse) => {
+			if (reverse) {
+				for (let i = from; i >= 0; i--) {
+					if (i === 0) {
+						return false;
+					}
+					else if (str.charAt(i) === ' ') {
+						return i;
+					}
+				}
+			}
+			else {
+				for (from; from < str.length; from++) {
+					if (str.charAt(from) === ' ') {
+						return from;
+					}
+				}
+				return from;
+			}
+		};
+
+		let charsLeft = str.length;
+		let last = false;
+		do {
+			const spaceBeforeCols = findSpace(str, cols, true);
+			const spaceAfterCols = charsLeft >= cols ? findSpace(str, cols) : null;
+
+			let breakIdx;
+			if (charsLeft <= cols) {
+				last = true;
+			}
+			else {
+				breakIdx = spaceBeforeCols ? spaceBeforeCols : spaceAfterCols;
+			}
+
+			if (last) { // <-- End of string
+				out += str;
+				charsLeft = 0;
+			}
+			else {
+				out += str.slice(0, breakIdx) + '\n';
+				str = str.slice(breakIdx + 1);
+				charsLeft = str.length;
+			}
+		} while (charsLeft > 0);
+		
+		return out;
   },
 
   reverseString: function(str) {
-
+		const charArr = str.split('');
+		let out = '';
+		for (let i = charArr.length - 1; i >= 0; i--) {
+			out += charArr[i];
+		}
+		return out;
   }
 };
